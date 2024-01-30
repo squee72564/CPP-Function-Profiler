931710a3f8c07cf90cac46784f0086496e0b10c2
squee72564 <rodriguez.al183@gmail.com>
2024-01-30 08:12:09 -0800

    Separated initialization of static class members and function definitions into separate .cpp file
    We cannot initialize the static class members the previous way within the .hpp file if we use any
    c++ standard below 17. These changes will allow the profiler to compile with c++11 or above, but
    requires a .cpp file for the static class member initializations.

--- Profiler.hpp
+++ Profiler.hpp
@@ -36,21 +36,8 @@ public:
     const char * name;
   };
   
-  inline APIProfiler(ThreadInfo * threadInfo) {
-    LARGE_INTEGER start;
-    QueryPerformanceCounter( & start);
-    m_start = start.QuadPart;
-    m_threadInfo = threadInfo;
-  }
-
-  inline ~APIProfiler() {
-    LARGE_INTEGER end;
-    QueryPerformanceCounter(&end);
-    m_threadInfo->accumulator += (end.QuadPart - m_start);
-    m_threadInfo->hitCount++;
-    if (end.QuadPart - m_threadInfo->lastReportTime > s_reportInterval)
-      Flush(end.QuadPart);
-  }
+  APIProfiler(ThreadInfo * threadInfo);
+  ~APIProfiler();
   
 private:
 
@@ -59,40 +46,9 @@ private:
   static float s_ooFrequency;
   static INT64 s_reportInterval;
   
-  void Flush(INT64 end) {
-    if (s_reportInterval == 0) {
-      LARGE_INTEGER freq;
-      QueryPerformanceFrequency(&freq);
-      s_ooFrequency = 1.0f / freq.QuadPart;
-      MemoryBarrier();
-      s_reportInterval = static_cast < INT64 > (freq.QuadPart * 1.0f);
-    }
-
-    if (m_threadInfo->lastReportTime == 0) {
-      m_threadInfo->lastReportTime = m_start;
-      return;
-    }
-
-    float interval = (end - m_threadInfo->lastReportTime) * s_ooFrequency;
-    float measured = m_threadInfo->accumulator * s_ooFrequency;
-
-    printf("TID 0x%lx time spent in \"%s\": %.0f/%.0f microsec %.1f%% %lldx\n",
-      GetCurrentThreadId(),
-      m_threadInfo->name,
-      measured * 1e6,
-      interval * 1e6,
-      100.0f * measured / interval,
-      m_threadInfo->hitCount);
-
-    m_threadInfo->lastReportTime = end;
-    m_threadInfo->accumulator = 0;
-    m_threadInfo->hitCount = 0;
-  }
+  void Flush(INT64 end);
 };
 
-// Initialize static class members
-inline float APIProfiler::s_ooFrequency = 0;
-inline INT64 APIProfiler::s_reportInterval = 0;
 
 #endif //API_PROFILER_WINDOWS
 
@@ -109,25 +65,8 @@ public:
     const char * name;
   };
 
-  inline APIProfiler(ThreadInfo * threadInfo) {
-    clock_gettime(CLOCK_MONOTONIC, &m_start);
-    m_threadInfo = threadInfo;
-  }
-
-  inline ~APIProfiler() {
-    timespec end;
-    clock_gettime(CLOCK_MONOTONIC, &end);
-
-    long long startNanoSec = m_start.tv_sec * 1e9 + m_start.tv_nsec;
-    long long endNanoSec = end.tv_sec * 1e9 + end.tv_nsec;
-    long long lastReportNanoSec = m_threadInfo->lastReportTime.tv_sec * 1e9 + m_threadInfo->lastReportTime.tv_nsec;
-
-    m_threadInfo->accumulator += (endNanoSec - startNanoSec) / 1000LL;
-    m_threadInfo->hitCount++;
-
-    if (((endNanoSec - lastReportNanoSec) / 1000LL) > s_reportInterval)
-      Flush(end);
-  }
+  APIProfiler(ThreadInfo * threadInfo);
+  ~APIProfiler();
 
 private:
 
@@ -135,37 +74,9 @@ private:
   timespec m_start;
   static long long s_reportInterval;
   
-  void Flush(timespec end) {
-    if (s_reportInterval == 0) {
-      s_reportInterval = static_cast < long long > (1e6 * 1.0f);
-    }
-
-    if (m_threadInfo->lastReportTime.tv_sec == 0 && m_threadInfo->lastReportTime.tv_nsec == 0) {
-      m_threadInfo->lastReportTime = m_start;
-      return;
-    }
-
-    double interval = static_cast < double > ((end.tv_sec - m_threadInfo->lastReportTime.tv_sec) * 1e9 +
-      (end.tv_nsec - m_threadInfo->lastReportTime.tv_nsec)) / 1000.0f;
-
-    double measured = m_threadInfo->accumulator;
-
-    printf("TID 0x%lx time spent in \"%s\": %.0f/%.0f microsec %.1f%% %lldx\n",
-      (long) pthread_self(),
-      m_threadInfo->name,
-      measured,
-      interval,
-      100.0f * measured / interval,
-      m_threadInfo->hitCount);
-
-    m_threadInfo->lastReportTime = end;
-    m_threadInfo->accumulator = 0;
-    m_threadInfo->hitCount = 0;
-  }
+  void Flush(timespec end);
 };
 
-// Initialize static class members
-inline long long APIProfiler::s_reportInterval = 0;
 
 #endif //API_PROFILER_UNIX
 
